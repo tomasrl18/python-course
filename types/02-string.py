db32c26e047b9abf43ad5c7fa9d481abf4bd9e97
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-16 19:24:40 +0200

    Strings

---
+++ types/02-string.py
@@ -0,0 +1,14 @@
+course_name = 'Ultimate Python'
+description = """
+Ultimate Python
+This course provides you all the details
+that you need to learn to find a job as
+programmer
+"""
+
+print(len(course_name))
+print(course_name[0])
+print(course_name[0:8])
+print(course_name[9:])
+print(course_name[:8])
+print(course_name[:])
