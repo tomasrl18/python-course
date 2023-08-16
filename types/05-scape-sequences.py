754a7cf9c6aab8e98ac14a4ea6c297ee108b9aa0
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-16 20:05:59 +0200

    Sequences of scape

---
+++ types/05-scape-sequences.py
@@ -0,0 +1,12 @@
+# This is a comment
+# \"
+# \'
+# \\
+# \n
+
+course = 'Ultimate "Python"'
+course2 = "Ultimate \"Python\""
+course3 = "Ultimate \nPython"
+print(course)
+print(course2)
+print(course3)
