20c91ba183d27c3d3e4788552147175e54aec74b
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-16 19:10:04 +0200

    Types to define a variable

---
+++ types/01-variables.py
@@ -0,0 +1,10 @@
+course_name = "Ultimate Python"
+name1 = "Hello"
+COURSE_NAME = "World"
+CourseName = "Chanchito"
+
+print(course_name, name1, COURSE_NAME, CourseName)
+
+students = 5000
+score = 9.9
+published = True
