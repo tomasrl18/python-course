1fca915457c73c9c35a57abb9e0c571f29520bc3
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-18 08:56:48 +0200

    Math module

---
+++ types/07-function-numbers.py
@@ -0,0 +1,13 @@
+import math
+
+print(round(1.3))
+print(round(1.353245, 2))
+
+print(abs(-77))
+
+print(math.ceil(1.1))
+print(math.floor(1.9999))
+print(math.isnan(23))
+# print(math.isnan("ggg"))
+print(math.pow(10, 3))
+print(math.sqrt(9))
