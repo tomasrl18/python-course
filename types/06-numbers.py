e51887e0f065d92a35cd3a735a995e98e93d557e
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-18 08:51:40 +0200

    Numbers

---
+++ types/06-numbers.py
@@ -0,0 +1,15 @@
+number = 1 # integer
+decimal = 1.2 # float
+imaginary = 2 + 2j # 2 + 2i
+
+number = number + 2
+number += 2
+
+print('Numero', number)
+print(1 + 2)
+print(1 - 1)
+print(1 * 5)
+print(5 / 5)
+print(5 // 5)
+print(5 % 5)
+print(2 ** 3)
