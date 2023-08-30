56e40bbb3851e78298c5187ddd03256877e24c99
tomasrl18 <tomaslopezraigal@gmail.com>
2023-08-30 14:10:36 +0200

    ForElse

--- flow-control/06-for.py
+++ flow-control/06-for.py
@@ -1,2 +1,8 @@
+find = 10
 for number in range(5):
-    print(number, number * 'Hola mundo')
+    print(number)
+    if number == find:
+        print("Found", find)
+        break
+else:
+    print("Not Found")
