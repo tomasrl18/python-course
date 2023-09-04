b6161d94952fc8b19999498e67b1b7c92d9b88d1
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-04 14:48:15 +0200

    Adding and deleting in lists

---
+++ advanced-types/06-adding-deleting.py
@@ -0,0 +1,18 @@
+mascots = [
+    "Pelusa",
+    "Pulga",
+    "Felipe",
+    "Pulga",
+    "Chanchito feliz",
+    "Raigal"
+]
+
+mascots.insert(1, "Melvin")
+mascots.append("Chanchito triste")
+
+mascots.remove("Pulga")
+mascots.pop()
+mascots.pop(1)
+del mascots[0]
+mascots.clear()
+print(mascots)
