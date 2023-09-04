153b99903a7200fe3ad8746878c17216fb9c3bef
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-04 15:00:32 +0200

    Ordering lists

---
+++ advanced-types/07-ordering-lists.py
@@ -0,0 +1,20 @@
+numbers = [2, 4, 1, 45, 75, 22]
+
+#numbers.sort()
+#numbers.sort(reverse=True)
+numbers2 = sorted(numbers, reverse=True)
+print(numbers)
+print(numbers2)
+
+users = [[4, "Chachito"], [1, "Felipe"], [5, "Pulga"]]
+users2 = [["Chachito", 4], ["Felipe", 1], ["Pulga", 5]]
+users.sort()
+users2.sort()
+print(users)
+print(users2)
+
+def order(element):
+    return element[1]
+
+users2.sort(key=order, reverse=True)
+print(users2)
