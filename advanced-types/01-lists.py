3b7dc0899dd6cbd2ccb13465ee7698cce38436ca
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-04 13:51:46 +0200

    Lists

---
+++ advanced-types/01-lists.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+words = ["Piggy", "Happy"]
+happyWords = ["Piggy", "Happy", "Felipe", "Student"]
+booleans = [True, False, True, False]
+matrix = [[0, 1], [1, 2], [2, 3], [3, 4]]
+zeros = [0] * 10
+alphaNumerical = numbers + letters
+ranges = list(range(1, 11))
+chars = list("Hello world!")
+print(chars)
