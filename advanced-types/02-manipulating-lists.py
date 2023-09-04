811575374008ac43ad38a0a74447b7e6f47f7e12
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-04 14:16:56 +0200

    Manipulating Lists

---
+++ advanced-types/02-manipulating-lists.py
@@ -0,0 +1,10 @@
+mascots = ["Raigal", "Pelusa", "Pulga", "Copito"]
+print(mascots[0])
+mascots[0] = "Bicho"
+#print(mascots)
+#print(mascots[2:])
+#print(mascots[-1])
+#print(mascots[1:2:2])
+
+numbers = list(range(21))
+print(numbers[::2])
