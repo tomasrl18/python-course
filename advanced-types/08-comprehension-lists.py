4e9bb0723c13ac0b3df67a4e280473e04ea5c26f
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-05 14:40:12 +0200

    Comprehension lists

---
+++ advanced-types/08-comprehension-lists.py
@@ -0,0 +1,19 @@
+users = [
+    ["Chachito", 4],
+    ["Felipe", 1],
+    ["Pulga", 5]
+]
+
+#names = []
+#for user in users:
+#    names.append(user[0])
+#print(names)
+
+#names = [user[0] for user in users]
+#print(names)
+
+#names = [user for user in users if user[1] > 2]
+#print(names)
+
+names = [user[0] for user in users if user[1] > 2]
+print(names)
