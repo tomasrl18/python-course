b11d39ed358a0a9a8a6e2e2a27897bf8e57f3df3
tomasrl18 <tomaslopezraigal@gmail.com>
2023-09-07 13:37:52 +0200

    Sets

---
+++ advanced-types/10-sets.py
@@ -0,0 +1,10 @@
+# set significa grupo o conjunto
+
+first = { 1, 1, 2, 2, 3, 4 }
+second = [3, 4, 5]
+second = set(second)
+
+#print(first | second)
+#print(first & second)
+#print(first - second)
+print(first ^ second)
